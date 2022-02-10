e4de946e9adc4a9ad17eb25b90e8c76403c852f7
sandra-ntsoele <sandra.ntsoele@pm.me>
2022-02-10 19:47:00 +0200

    refactor: add Main method to the program's entry file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
